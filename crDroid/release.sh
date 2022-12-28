d68e16bf6366ed7fbec89147ac74f1c12ec1581d
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2022-12-28 16:56:15 +0800

    crDroid: Add support for grus

--- crDroid/release.sh
+++ crDroid/release.sh
@@ -42,7 +42,7 @@ if [ "$DEVICE" = "d1x" ] ; then
             --arg firmware "https://sourceforge.net/projects/firmware-samsung-note10-5g/files/" \
             --arg modem "" \
             --arg bootloader "" \
-            --arg recovery "https://sourceforge.net/projects/projects/crdroid-samsung-note10-5g/files/crdroid-9/recovery.img/download" \
+            --arg recovery "https://sourceforge.net/projects/$PROJECT/files/crdroid-9/recovery.img/download" \
             --arg paypal "" \
             --arg telegram "" \
             --arg dt "https://github.com/Rocky7842/android_device_samsung_d1x" \
@@ -51,6 +51,33 @@ if [ "$DEVICE" = "d1x" ] ; then
             '$ARGS.named'
     )
     wrapped_response=$(jq -n --argjson response "[$response]" '$ARGS.named')
+elif [ "$DEVICE" = "grus" ] ; then
+    PROJECT="crdroid-xiaomi-9-se"
+
+    response=$(jq -n --arg maintainer "Rocky7842" \
+            --arg oem "Xiaomi" \
+            --arg device "Mi 9 SE" \
+            --arg filename $FILENAME \
+            --arg download "https://sourceforge.net/projects/$PROJECT/files/crdroid-9/$ROM/download" \
+            --arg timestamp $TIMESTAMP \
+            --arg md5 $MD5 \
+            --arg sha256 $SHA256 \
+            --arg size $SIZE \
+            --arg version $VERSION \
+            --arg buildtype "Monthly" \
+            --arg forum "https://forum.xda-developers.com/t/rom-unofficial-13-crdroid-9-for-mi-9-se-grus-encryption.4533353/" \
+            --arg gapps "http://downloads.codefi.re/jdcteam/javelinanddart/gapps" \
+            --arg firmware "https://androidfilehost.com/?fid=2981970449027569929" \
+            --arg modem "" \
+            --arg bootloader "" \
+            --arg recovery "https://sourceforge.net/projects/$PROJECT/files/crdroid-9/recovery.img/download" \
+            --arg paypal "" \
+            --arg telegram "" \
+            --arg dt "https://github.com/Rocky7842/android_device_xiaomi_grus" \
+            --arg common-dt "" \
+            --arg kernel "https://github.com/Rocky7842/android_kernel_xiaomi_sdm710" \
+            '$ARGS.named'
+    )
 fi
 
 echo "$wrapped_response" > crDroid/$MAINVERSION/$DEVICE.json
