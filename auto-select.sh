394ef15c86b374e9cc875fcd09c612817046a195
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-01-03 13:29:57 +0800

    crDroid: release.sh: Pull timestamp from build generated json

--- auto-select.sh
+++ auto-select.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 if [ -z "$1" ] ; then
-    echo "Usage: $0 <ota zip> <optional:json of the build>"
+    echo "Usage: $0 <ota zip>"
     exit 1
 fi
 
 ROM="$1"
-OLDJSON="$2"
 
 FILENAME=$(basename $ROM)
-
 ROMNAME=$(echo "$FILENAME" | cut -f1 -d '-' )
-
 if [ "$ROMNAME" = "lineage" ] ; then
     sh ./LineageOS/release.sh $ROM
 elif [ "$ROMNAME" = "crDroidAndroid" ] ; then
-    sh ./crDroid/release.sh $ROM $OLDJSON
+    sh ./crDroid/release.sh $ROM
 fi
