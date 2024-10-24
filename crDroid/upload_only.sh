56bc64f18c55033dd3a0bc044042e3fe6ae51d3d
Rocky7842 <eric.rocky7842@gmail.com>
2024-10-24 17:45:22 +0800

    crDroid: Add a script to upload old files

---
+++ crDroid/upload_only.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+    echo "Usage: $0 <ota zip>"
+    exit 1
+fi
+
+ROM="$1"
+ROM_DIR=$(dirname "$ROM")
+
+ROMFILENAME=$(basename $ROM)
+CODENAME=$(echo $ROMFILENAME | cut -f4 -d '-')
+
+ANDROID_VERSION=$(echo $ROMFILENAME | cut -f2 -d '-')
+if [ "${ANDROID_VERSION##*.}" -eq 0 ]; then
+    ANDROID_VERSION=${ANDROID_VERSION%.*}
+fi
+
+VERSION=$(echo $ROMFILENAME | cut -d '-' -f 5 | sed 's/v\([0-9]\+\.[0-9]\+\).*/\1/')
+MAINVERSION=$(echo $VERSION | cut -f1 -d '.')
+
+MAINTAINER="Rocky7842"
+# Remember to also setup GITHUB_ACCESS_TOKEN for use in the script!
+
+RECOVERY_IN_BOOT=false
+
+if [ "$CODENAME" = "d1x" ] ; then
+    OEM="Samsung"
+    DEVICE="Galaxy Note10 5G"
+    BUILDTYPE="Monthly"
+    FIRMWARE="https://sourceforge.net/projects/firmware-samsung-note10-5g/files/"
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_samsung_d1x"
+    COMMON_DEVICETREE="https://github.com/Rocky7842/android_device_samsung_exynos9820-common"
+    KERNEL="https://github.com/Rocky7842/android_kernel_samsung_exynos9820"
+    if [ "$MAINVERSION" = "9" ] ; then
+        FORUM="https://xdaforums.com/t/4530391/"
+    else
+        FORUM=""
+    fi
+elif [ "$CODENAME" = "grus" ] ; then
+    OEM="Xiaomi"
+    DEVICE="Mi 9 SE"
+    BUILDTYPE="Monthly"
+    FIRMWARE="https://androidfilehost.com/?fid=2981970449027569929"
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_grus"
+    COMMON_DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_sdm710-common"
+    KERNEL="https://github.com/Rocky7842/android_kernel_xiaomi_sdm710"
+    if [ "$MAINVERSION" = "9" ] ; then
+        FORUM="https:/xdaforums.com/t/4533353/"
+    elif [ "$MAINVERSION" = "10" ] ; then
+        FORUM="https://xdaforums.com/t/4639898/"
+    else
+        FORUM=""
+    fi
+elif [ "$CODENAME" = "xmsirius" ] ; then
+    OEM="Xiaomi"
+    DEVICE="Mi 8 SE"
+    BUILDTYPE="Monthly"
+    FIRMWARE=""
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_xmsirius"
+    COMMON_DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_sdm710-common"
+    KERNEL="https://github.com/Rocky7842/android_kernel_xiaomi_sdm710"
+    if [ "$MAINVERSION" = "9" ] ; then
+        FORUM="https://xdaforums.com/t/4535219/"
+    elif [ "$MAINVERSION" = "10" ] ; then
+        FORUM="https://xdaforums.com/t/4639873/"
+    else
+        FORUM=""
+    fi
+elif [ "$CODENAME" = "X01AD" ] ; then
+    OEM="Asus"
+    DEVICE="ZenFone Max M2"
+    BUILDTYPE="Monthly"
+    FIRMWARE=""
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_asus_X01AD"
+    COMMON_DEVICETREE=""
+    KERNEL="https://github.com/Rocky7842/android_kernel_asus_X01AD"
+    if [ "$MAINVERSION" = "10" ] ; then
+        FORUM="https://xdaforums.com/t/4643500/"
+    else
+        FORUM=""
+    fi
+elif [ "$CODENAME" = "obiwan" ] ; then
+    OEM="Asus"
+    DEVICE="ROG Phone 3"
+    BUILDTYPE="Monthly"
+    FIRMWARE=""
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_asus_obiwan"
+    COMMON_DEVICETREE=""
+    KERNEL="https://github.com/Rocky7842/android_kernel_asus_sm8250"
+    if [ "$MAINVERSION" = "10" ] ; then
+        FORUM="https://xdaforums.com/t/4681840/"
+    else
+        FORUM=""
+    fi
+elif [ "$CODENAME" = "psyche" ] ; then
+    OEM="Xiaomi"
+    DEVICE="12X"
+    BUILDTYPE="Monthly"
+    FIRMWARE=""
+    MODEM=""
+    BOOTLOADER=""
+    DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_psyche"
+    COMMON_DEVICETREE="https://github.com/Rocky7842/android_device_xiaomi_sm8250-common"
+    KERNEL="https://github.com/Rocky7842/android_kernel_xiaomi_sm8250"
+    if [ "$MAINVERSION" = "10" ] ; then
+        FORUM="https://xdaforums.com/t/4682358/"
+    else
+        FORUM=""
+    fi
+
+    RECOVERY_IN_BOOT=true
+fi
+
+TAG="$CODENAME-crDroid-$VERSION"
+
+git pull
+git tag -d $TAG
+git tag $TAG
+git push origin main -f --tags
+
+RELEASE_RESPONSE=$(curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{
+       "tag_name": "'"$TAG"'",
+       "target_commitish": "main",
+       "name": "'"${ROMFILENAME%.zip}"'",
+       "body": "ROM: crDroid<br>Codename: '"$CODENAME"'<br>Device: '"$DEVICE"' <br>crDroid version: '"$MAINVERSION"' <br>Android version: '"$ANDROID_VERSION"' <br>Variant: VANILLA",
+       "draft": false,
+       "prerelease": false
+     }' \
+     https://api.github.com/repos/$MAINTAINER/OTA_provider/releases)
+
+echo ""
+echo "------Release posted------"
+echo ""
+
+RELEASE_ID=$(echo "$RELEASE_RESPONSE" | jq -r .id)
+UPLOAD_URL="https://uploads.github.com/repos/$MAINTAINER/OTA_provider/releases/$RELEASE_ID/assets"
+
+curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+     -H "Content-Type: application/octet-stream" \
+     --data-binary @"$ROM" \
+     "$UPLOAD_URL?name=$ROMFILENAME"
+
+echo ""
+echo "------ROM uploaded------"
+echo ""
+
+if [ "$RECOVERY_IN_BOOT" = false ] ; then
+    RECOVERY="${ROM_DIR}/recovery.img"
+    FILES="$RECOVERY"
+else
+    BOOT="${ROM_DIR}/boot.img"
+    VENDOR_BOOT="${ROM_DIR}/vendor_boot.img"
+    DTBO="${ROM_DIR}/dtbo.img"
+    FILES="$BOOT $VENDOR_BOOT $DTBO"
+fi
+
+for FILE in $FILES; do
+    FILENAME=$(basename "$FILE")
+    curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+         -H "Content-Type: application/octet-stream" \
+         --data-binary @"$FILE" \
+         "$UPLOAD_URL?name=$FILENAME"
+done
+
+echo ""
+echo "------Recovery uploaded------"
+echo ""
