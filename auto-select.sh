6232c796ad889c1f9fff4c681d6d25fde998d467
Rocky7842 <eric.rocky7842@gmail.com>
2026-05-30 23:19:11 +0800

    crDroid: Merge the usage of only uploading files

--- auto-select.sh
+++ auto-select.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 if [ -z "$1" ] ; then
-    echo "Usage: $0 <ota zip>"
+    echo "Usage: $0 <ota zip> upload-file-only=<true or false>"
     exit 1
 fi
 
 ROM="$1"
+case "$2" in
+    "upload-file-only=true"|"upload-file-only=false"|"upload-file-only="|"")
+        # Pass argument if valid
+        UPLOAD_FILE_ONLY="$2"
+        ;;
+    *)
+        echo "Error: invalid argument: $2"
+        exit 1
+        ;;
+esac
 
 FILENAME=$(basename $ROM)
 ROMNAME=$(echo "$FILENAME" | cut -f1 -d '-' )
 if [ "$ROMNAME" = "lineage" ] ; then
-    sh ./LineageOS/release.sh $ROM
+    sh ./LineageOS/release.sh $ROM $UPLOAD_FILE_ONLY
 elif [ "$ROMNAME" = "crDroidAndroid" ] ; then
-    sh ./crDroid/release.sh $ROM
+    sh ./crDroid/release.sh $ROM $UPLOAD_FILE_ONLY
 fi
 
 echo "Publish OTA update successfully."
